c29968a5f0d95e4c3c7e3e8e2d6b33fd45205b2b
AbdullahM0hamed <25087116+AbdullahM0hamed@users.noreply.github.com>
2021-09-21 00:41:54 +0100

    Call super.OnViewBound

--- AvatarChanger/src/main/kotlin/com/aliucord/plugins/settings/EditAvatar.kt
+++ AvatarChanger/src/main/kotlin/com/aliucord/plugins/settings/EditAvatar.kt
@@ -12,6 +12,8 @@ data class EditAvatar(
 ) : SettingsPage() {
 
     override fun onViewBound(view: View) {
+        super.onViewBound(view)
+
         if (guild == null && user == null) {
             Utils.showToast(view.context, "Invalid User/Server")
             activity?.onBackPressed()
