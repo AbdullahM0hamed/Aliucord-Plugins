9abfd94474d05e233200e6d9e40d29c6ade848ce
AbdullahM0hamed <25087116+AbdullahM0hamed@users.noreply.github.com>
2021-09-21 21:31:10 +0100

    Fine, I'll do it myself ~me, after failing to use DownloadUtils

--- AvatarChanger/src/main/kotlin/com/aliucord/plugins/settings/EditAvatar.kt
+++ AvatarChanger/src/main/kotlin/com/aliucord/plugins/settings/EditAvatar.kt
@@ -3,12 +3,19 @@ package com.aliucord.plugins.settings
 import android.os.Environment
 import android.view.View
 import android.widget.LinearLayout
+import com.aliucord.Http
 import com.aliucord.Utils
 import com.aliucord.fragments.SettingsPage
 import com.aliucord.views.Button
 import com.discord.models.guild.Guild
 import com.discord.models.user.User
-import com.discord.utilities.file.DownloadUtils
+import com.discord.utilities.color.ColorCompat
+import com.discord.utilities.user.UserUtils
+import com.lytefast.flexinput.R
+
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 
 data class EditAvatar(
     val guild: Guild? = null,
@@ -25,7 +32,7 @@ data class EditAvatar(
         }
 
         setActionBarTitle("Edit Avatar")
-        setActionBarSubtitle(guild?.name ?: user!!.username)
+        setActionBarSubtitle(guild?.name ?: user!!.getUserNameWithDiscriminator())
         linearLayout.addView(ProfileWidget(
             ctx=view.context, 
             guild=guild, 
@@ -37,16 +44,7 @@ data class EditAvatar(
 
         Button(view.context).apply {
             text = "Download Current Avatar"
-            setOnClickListener {
-                DownloadUtils.downloadFile(
-                    view.context,
-                    guild?.icon ?: user!!.avatar,
-                    guild?.name ?: user!!.username + ".png",
-                    view.context.getExternalFilesDir(
-                        Environment.DIRECTORY_DOWNLOADS
-                    )
-                )
-            }
+            setOnClickListener { downloadAvatar(view.context) }
 
             buttons.addView(this)
         }
@@ -58,5 +56,42 @@ data class EditAvatar(
         }
 
         linearLayout.addView(buttons)
+    }
+
+    private fun downloadAvatar(context: Context) {
+        Utils.threadPool.execute(() -> {
+            val url = guild?.icon ?: user!!.avatar
+            val file = File(
+                guild?.name ?: user!!.username + ".png",
+                context.getExternalFilesDir(
+                    Environment.DIRECTORY_DOWNLOADS
+                )
+            )
+
+            if (file.exists()) {
+                Utils.showToast(
+                    context,
+                    "${guild?.name ?: user!!.username}.png already downloaded"
+                )
+                return
+            }
+
+            try {
+                val res = Http.Request(url).execute()
+                FileOutputStream(file).use {
+                    res.pipe(it)
+                    Utils.showToast(
+                        context,
+                        "${file.absolutePath} downloaded"
+                    )
+                    Utils.mainThread.post(callback)
+                }
+            } catch (e: IOException) {
+                Utils.showToast(context, "Something went wrong")
+                if (file.exists()) {
+                    file.delete()
+                }
+            }
+        })
     }
 }
