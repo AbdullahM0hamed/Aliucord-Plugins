43c1ad70c44a11b1aee185014c42b14ebea6256f
AbdullahM0hamed <25087116+AbdullahM0hamed@users.noreply.github.com>
2021-12-08 17:56:38 +0000

    Attempt to hook MessageContent's getTextContent method

--- TextReplace/src/main/kotlin/com/aliucord/plugins/TextReplace.kt
+++ TextReplace/src/main/kotlin/com/aliucord/plugins/TextReplace.kt
@@ -7,7 +7,9 @@ import com.aliucord.Utils
 import com.aliucord.annotations.AliucordPlugin
 import com.aliucord.api.SettingsAPI
 import com.aliucord.entities.Plugin
+import com.aliucord.patcher.PreHook
 import com.aliucord.plugins.settings.TextReplaceSettings
+import com.discord.widgets.chat.MessageContent
 
 @AliucordPlugin
 class TextReplace : Plugin() {
@@ -30,6 +32,23 @@ class TextReplace : Plugin() {
 
     override fun start(context: Context) {
         mSettings = settings
+        patcher.patch(MessageContent::class.java.getDeclaredMethod("sendMessage"),
+        PreHook { callFrame ->
+            val textContent = MessageContent::class.java.getDeclaredField("textContent").apply {
+                isAccessible = true
+            }
+            var text = textContent.get(callFrame.thisObject) as String
+            val map = TextReplace.mSettings.getObject(
+                "replaceMap",
+                mutableMapOf<String, String>()
+            )
+
+            map.toList().forEach { old, new ->
+                text = text.replace(old, new)
+            }
+
+            callFrame.result = text
+        }
     }
 
     override fun stop(context: Context) = patcher.unpatchAll()
